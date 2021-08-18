e1346d8b17463eacbe89580533bdd77f74c06e02
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-08-17 22:32:24 -0500

    a basic/empty frame was implemented

---
+++ src/BasicJFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class BasicJFrame {
+    public static void main(String[] args) {
+        JFrame frame =  new JFrame();
+        frame.setBounds(10, 20, 300, 400);
+        //render
+        frame.setVisible(true);
+
+        //exit the app once the frame is closed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
