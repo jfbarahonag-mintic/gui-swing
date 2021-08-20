8dba84d75da995df1c471ec719f4e87961a8d8cb
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-08-19 21:40:32 -0500

    form resized to 600px at height

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class App {
     public static void main(String[] args) throws Exception {
         Form f =  new Form();
-        f.setBounds(0, 0, 400, 300);
+        f.setBounds(0, 0, 400, 600);
         f.setVisible(true);
         f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
